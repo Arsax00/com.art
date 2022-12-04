1cb880089a3d9fe71d60db1e953060a36ec5dd67
Miguel Ángel Rodríguez Dalí <migueadali@gmail.com>
2022-12-04 12:50:15 +0100

    Dos clases faltantes hechas

---
+++ com.art/src/main/java/models/Proveedores.java
@@ -0,0 +1,122 @@
+package models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "proveedores")
+
+public class Proveedores implements Serializable {
+	@Id
+	@Column(name = "cif")
+	private String cif;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "direccion")
+	private String direccion;
+
+	@Column(name = "telefono")
+	private int telefono;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "web")
+	private String web;
+
+	@Column(name = "fax")
+	private int fax;
+
+	@Column(name = "observaciones")
+	private String observaciones;
+
+	public Proveedores(String cif, String nombre, String dir, int telf, String email, String web, int fax,
+			String observaciones) {
+		super();
+		this.cif = cif;
+		this.nombre = nombre;
+		this.direccion = dir;
+		this.telefono = telf;
+		this.email = email;
+		this.web = web;
+		this.fax = fax;
+		this.observaciones = observaciones;
+	}
+
+	@Override
+	public String toString() {
+		return "Proveedores [id=" + cif + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono
+				+ ", email=" + email + ", web=" + web + ", fax=" + fax + ", observaciones=" + observaciones + "]";
+	}
+
+	public String getCif() {
+		return cif;
+	}
+
+	public void setCif(String cif) {
+		this.cif = cif;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public int getFax() {
+		return fax;
+	}
+
+	public void setFax(int fax) {
+		this.fax = fax;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	}
+
+}
