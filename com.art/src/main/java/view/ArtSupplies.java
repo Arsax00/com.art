a1c620483bc0a79cbbb6fc333bb1d355733f83f0
Arsax00 <57747785+Arsax00@users.noreply.github.com>
2022-12-06 12:33:18 +0100

    Update ArtSupplies.java

--- com.art/src/main/java/view/ArtSupplies.java
+++ com.art/src/main/java/view/ArtSupplies.java
@@ -15,7 +15,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
-import javax.swing.JComboBox;
 
 public class ArtSupplies extends JFrame {
     int xMouse, yMouse;
